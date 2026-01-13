6f1a68a5e1f989fb1131567f6ffe260afd17753e
Isaac Mackle <isaac.mackle@gmail.com>
2026-01-13 15:25:27 +1300

    fix: form submission

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -15,54 +15,61 @@ type FormValues = {
     message: string;
 };
 
-function encodeForm(data: Record<string, any>) {
-    return new URLSearchParams(data).toString();
-}
-
 export default function ContactForm() {
-    const { register, handleSubmit, reset } = useForm<FormValues>();
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
 
-    const mutation = useMutation(
-        {
-            mutationFn: async (data: FormValues) => {
-                const body = encodeForm({
-                    'form-name': 'contact',
-                    ...data
-                });
+        const myForm = event.target;
+        const formData = new FormData(myForm);
 
-                const res = await fetch('/netlify-form.html', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body
-                });
-                if (!res.ok) throw new Error('Form submission failed');
-                return res;
-            },
-            onError: () => {
-                toast.error('Opps something went wrong. Please try again.');
-            },
-            onSuccess: () => {
-                toast.success('Thank you, we will be in touch!');
-                reset();
-            }
-        },
-        queryClient
-    );
-
-    const onSubmit = (data: FormValues) => {
-        mutation.mutate(data);
+        fetch('/netlify-form.html', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString()
+        })
+            .then(() => toast.success('Thank you, we will be in touch!'))
+            .catch(() =>
+                toast.error('Opps something went wrong. Please try again.')
+            );
     };
+
+    // const mutation = useMutation(
+    //     {
+    //         mutationFn: async (data: FormValues) => {
+    //             const body = encodeForm({
+    //                 'form-name': 'contact',
+    //                 ...data
+    //             });
+
+    //             const res = await fetch('/netlify-form.html', {
+    //                 method: 'POST',
+    //                 headers: {
+    //                     'Content-Type': 'application/x-www-form-urlencoded'
+    //                 },
+    //                 body
+    //             });
+    //             if (!res.ok) throw new Error('Form submission failed');
+    //             return res;
+    //         },
+    //         onError: () => {
+    //             toast.error('Opps something went wrong. Please try again.');
+    //         },
+    //         onSuccess: () => {
+    //             toast.success('Thank you, we will be in touch!');
+    //             reset();
+    //         }
+    //     },
+    //     queryClient
+    // );
+
+    // const onSubmit = (data: FormValues) => {
+    //     mutation.mutate(data);
+    // };
 
     return (
         <div>
             <form
-                onSubmit={(event) => {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    handleSubmit(onSubmit)(event);
-                }}
+                onSubmit={handleSubmit}
                 data-netlify-recaptcha="true"
                 data-netlify-honeypot="bot-field"
                 name="contact"
@@ -78,8 +85,8 @@ export default function ContactForm() {
                             className="bg-white text-black"
                             type="text"
                             placeholder="Name*"
-                            {...register('name')}
                             id="name"
+                            name="name"
                             required
                         />
                     </Field>
@@ -90,7 +97,7 @@ export default function ContactForm() {
                         className="bg-white text-black"
                         placeholder="Email*"
                         type="email"
-                        {...register('email')}
+                        name="email"
                         id="email"
                         required
                     />
@@ -101,8 +108,8 @@ export default function ContactForm() {
                         className="bg-white text-black"
                         placeholder="Phone*"
                         type="text"
-                        {...register('phone')}
                         id="phone"
+                        name="phone"
                         required
                     />
                     <FieldLabel htmlFor="message" className="sr-only">
@@ -111,18 +118,13 @@ export default function ContactForm() {
                     <Textarea
                         className="bg-white text-black"
                         placeholder="Message*"
-                        {...register('message')}
+                        name="message"
                         id="message"
                         required
                     />
                     <div data-netlify-recaptcha="true" />
-                    <Button
-                        type="submit"
-                        disabled={mutation.isPending}
-                        variant="default"
-                        className="w-fit"
-                    >
-                        {mutation.isPending ? <Spinner /> : 'Submit'}
+                    <Button type="submit" variant="default" className="w-fit">
+                        Submit
                     </Button>
                 </FieldGroup>
             </form>
